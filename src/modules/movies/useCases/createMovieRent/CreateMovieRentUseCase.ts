60cd6d12ec127b7341c666bca7166f8e10c854f8
ManoelEric1212 <manoeleric59@gmail.com>
2023-03-28 13:17:19 -0300

    feat(movieRent): :construction: Adicionando controller, DTO e useCase
    Adicionando estrtura de DTO, Controller e UseCase para movieRent

---
+++ src/modules/movies/useCases/createMovieRent/CreateMovieRentUseCase.ts
@@ -0,0 +1,44 @@
+import { AppError } from "../../../../errors/AppError";
+import { prisma } from "../../../../prisma/client";
+import { CreateMovieRentDTO } from "../../dtos/CreateMovieRentDTO";
+
+export class CreateMovieRentUseCase {
+  async execute({movieId, userId}: CreateMovieRentDTO): Promise<void>{
+    // Verifica se filme existe
+    const movieExists = await prisma.movie.findUnique({
+      where:{
+        id: movieId
+      }
+    })
+    if(!movieExists){
+      throw new AppError("Movie does not exists!")
+    }
+    // Verificar se o filme já está asoociado a um usuário 
+    const movieAlreadyRented = await prisma.movieRent.findFirst({
+      where: {
+        movieId: movieId
+      }
+    })
+    if(movieAlreadyRented){
+      throw new AppError("Movie already rented!");
+    }
+    // Verificar se o usuário existe
+    const userExists = await prisma.user.findUnique({
+      where:{
+        id: userId
+      }
+    });
+
+    if(!userExists){
+      throw new AppError("USer does not exists")
+    }
+    //criar a locação
+    await prisma.movieRent.create({
+      data:{
+        movieId,
+        userId,
+      },
+    })
+
+  }
+}
