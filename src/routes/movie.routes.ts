1cec1dd1bc237884605aa035c449f6f8799eaf7b
ManoelEric1212 <manoeleric59@gmail.com>
2023-03-29 08:17:14 -0300

    feat(movie): Adicionar controller movie getRelease]

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { CreateMovieController } from "../modules/movies/useCases/createMovie/CreateMovieController";
 import { CreateMovieRentController } from "../modules/movies/useCases/createMovieRent/CreateMovieRentController";
+import { GetMoviesController } from "../modules/movies/useCases/getMovies/GetMoviesController";
+import { GetMoviesByReleaseController } from "../modules/movies/useCases/getMoviesByReleaseDate/GetMoviesByReleaseDateContorller";
 
 const createMovieController = new CreateMovieController();
 const createMovieRentController = new CreateMovieRentController();
+const getMoviesByReleaseController = new GetMoviesByReleaseController();
+const getMoviesController = new GetMoviesController();
 const movieRoutes = Router();
 
 movieRoutes.post("/", createMovieController.handle);
+movieRoutes.get("/", getMoviesController.handle);
+movieRoutes.get("/release", getMoviesByReleaseController.handle);
 movieRoutes.post("/rent", createMovieRentController.handle);
+
 export {movieRoutes}
